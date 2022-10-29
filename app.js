477ae01c51064d60d982fecc92824933b8ce7aff
Konstantin Suvorov <huxt-huxt@yandex.ru>
2022-10-29 10:09:43 +0300

    ready_for_deploy

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config({ path: './.env' });
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express = require('express');
