67339edf9a91873a66c2736d82608f6c55011fe0
Konstantin Suvorov <huxt-huxt@yandex.ru>
2022-11-22 06:34:14 +0300

    fix cors

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -2,8 +2,8 @@ const rateLimit = require('express-rate-limit');
 
 const rateLimiter = rateLimit({
   windowMs: 2 * 60 * 1000,
-  max: 5,
-  message: 'Превышен лимит обращений: 5 обращений за 2 минуты, попробуйте позже',
+  max: 100,
+  message: 'Превышен лимит обращений: 100 обращений за 2 минуты, попробуйте позже',
   standardHeaders: true,
   legacyHeaders: false,
 });
